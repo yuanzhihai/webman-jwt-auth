dec52e5b2cd85393a2c08b073f74d37c26220504
yuanzhihai <396751927@qq.com>
2022-04-18 17:19:56 +0800

    init

--- src/user/AuthorizationUserInterface.php
+++ src/user/AuthorizationUserInterface.php
@@ -4,5 +4,5 @@ namespace yzh52521\JwtAuth\user;
 
 interface AuthorizationUserInterface
 {
-    public function getUserById($id): AuthorizationUserInterface;
+    public function getUserById($id);
 }
