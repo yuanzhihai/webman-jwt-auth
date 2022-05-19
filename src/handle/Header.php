08cef06dbc443bb5095185a46494d630ba19cb2b
yuanzhihai <396751927@qq.com>
2022-05-19 21:03:26 +0800

    update header get token

--- src/handle/Header.php
+++ src/handle/Header.php
@@ -4,15 +4,32 @@ namespace yzh52521\JwtAuth\handle;
 
 class Header extends RequestToken
 {
+    protected $header = 'authorization';
+
+    protected $prefix = 'bearer';
+
+    /**
+     * Attempt to parse the token from some other possible headers.
+     *
+     * @return string|null
+     */
+    protected function fromAltHeaders()
+    {
+        return \request()->header('HTTP_AUTHORIZATION') ?: \request()->header('REDIRECT_HTTP_AUTHORIZATION');
+    }
+
     public function handle()
     {
-        $authorization = request()->header('authorization');
+        $authorization = \request()->header($this->header) ?: $this->fromAltHeaders();
 
-        $token = '';
-        if ($authorization && preg_match('/Bearer\s*(\S+)\b/i', $authorization, $matches)) {
-            $token = $matches[1];
+        if ($authorization !== null) {
+            $position = strripos($authorization, $this->prefix);
+
+            if ($position !== false) {
+                $header = substr($authorization, $position + strlen($this->prefix));
+                return trim(strpos($header, ',') !== false ? strstr($header, ',', true) : $header);
+            }
         }
-
-        return $token;
+        return null;
     }
 }
