200f15e9224433447285e8019e8bdeecf1001dc9
yuanzhihai <396751927@qq.com>
2022-11-18 10:16:31 +0800

    修复自动续签

--- src/Jwt.php
+++ src/Jwt.php
@@ -31,7 +31,7 @@ class Jwt
      */
     protected $auth;
 
-    public function __construct(JwtAuth $jwt, $config, $manager)
+    public function __construct(JwtAuth $jwt,$config,$manager)
     {
         $this->auth    = $jwt;
         $this->config  = $config;
@@ -64,31 +64,31 @@ class Jwt
      * @param array $claims
      * @return $token
      */
-    public function make($identifier, array $claims = []): Token
+    public function make($identifier,array $claims = []): Token
     {
         $now = new DateTimeImmutable();
         if ($this->config->getLoginType() == 'mpo') {
-            $uniqid = uniqid($this->auth->getStore() . ':') . ":" . $identifier;
+            $uniqid = uniqid( $this->auth->getStore().':' ).":".$identifier;
         } else {
-            $uniqid = $this->auth->getStore() . ":" . $identifier;
+            $uniqid = $this->auth->getStore().":".$identifier;
         }
         $builder = $this->jwtConfiguration->builder()
-            ->issuedBy($this->config->getIss())
-            ->identifiedBy($uniqid)
-            ->issuedAt($now)
-            ->canOnlyBeUsedAfter($now)
-            ->expiresAt($this->getExpiryDateTime($now))
-            ->relatedTo($this->config->getSubject())
-            ->withClaim('store', $this->auth->getStore());
+            ->issuedBy( $this->config->getIss() )
+            ->identifiedBy( $uniqid )
+            ->issuedAt( $now )
+            ->canOnlyBeUsedAfter( $now )
+            ->expiresAt( $this->getExpiryDateTime( $now ) )
+            ->relatedTo( $this->config->getSubject() )
+            ->withClaim( 'store',$this->auth->getStore() );
 
-        foreach ($claims as $key => $value) {
-            $builder->withClaim($key, $value);
+        foreach ( $claims as $key => $value ) {
+            $builder->withClaim( $key,$value );
         }
 
-        $token = $builder->getToken($this->jwtConfiguration->signer(), $this->jwtConfiguration->signingKey());
+        $token = $builder->getToken( $this->jwtConfiguration->signer(),$this->jwtConfiguration->signingKey() );
 
         // 单点登录要把所有的以前生成的token都失效
-        if ($this->config->getLoginType() == 'sso') $this->auth->blackList->addTokenBlack($token,$this->config);
+        if ($this->config->getLoginType() == 'sso') $this->auth->blackList->addTokenBlack( $token,$this->config );
 
         return $token;
     }
@@ -102,7 +102,7 @@ class Jwt
     protected function getExpiryDateTime($now): DateTimeImmutable
     {
         $ttl = (string)$this->config->getExpires();
-        return $now->modify("+{$ttl} sec");
+        return $now->modify( "+{$ttl} sec" );
     }
 
     /**
@@ -114,9 +114,9 @@ class Jwt
     public function parseToken(string $token): Token
     {
         try {
-            return $this->jwtConfiguration->parser()->parse($token);
-        } catch (JwtException $e) {
-            throw new TokenInvalidException('Could not decode token: ' . $e->getMessage(), $e->getCode(), $e);
+            return $this->jwtConfiguration->parser()->parse( $token );
+        } catch ( JwtException $e ) {
+            throw new TokenInvalidException( 'Could not decode token: '.$e->getMessage(),$e->getCode(),$e );
         }
     }
 
@@ -129,49 +129,49 @@ class Jwt
      */
     public function validate(string $token): array
     {
-        $this->token = $this->parseToken($token);
+        $this->token = $this->parseToken( $token );
 
         $claims = $this->token->claims();
 
         $jwtConfiguration = $this->getValidateConfig();
 
         $jwtConfiguration->setValidationConstraints(
-            new SignedWith($jwtConfiguration->signer(), $jwtConfiguration->signingKey()),
-            new IdentifiedBy($claims->get('jti'))
+            new SignedWith( $jwtConfiguration->signer(),$jwtConfiguration->signingKey() ),
+            new IdentifiedBy( $claims->get( 'jti' ) )
         );
 
         $constraints = $jwtConfiguration->validationConstraints();
 
-        if (!$jwtConfiguration->validator()->validate($this->token, ...$constraints)) {
-            throw new TokenInvalidException('Token Signature could not be verified.', 500);
+        if (!$jwtConfiguration->validator()->validate( $this->token,...$constraints )) {
+            throw new TokenInvalidException( 'Token Signature could not be verified.',500 );
         }
 
         // 验证token是否存在黑名单
-        if ($this->manager->getBlacklistEnabled() && $this->auth->blackList->hasTokenBlack($claims, $this->config)) {
-            throw new TokenInvalidException('The token is in blacklist', 401);
+        if ($this->manager->getBlacklistEnabled() && $this->auth->blackList->hasTokenBlack( $claims,$this->config )) {
+            throw new TokenInvalidException( 'The token is in blacklist',401 );
         }
 
         $leeway = $this->config->getleeway();
 
-        if (Utils::isFuture($claims->get('nbf')->getTimestamp(), $leeway)) {
-            throw new TokenInvalidException('Not Before (nbf) timestamp cannot be in the future', 403);
+        if (Utils::isFuture( $claims->get( 'nbf' )->getTimestamp(),$leeway )) {
+            throw new TokenInvalidException( 'Not Before (nbf) timestamp cannot be in the future',403 );
         }
-        if (Utils::isFuture($claims->get('iat')->getTimestamp(), $leeway)) {
-            throw new TokenInvalidException('Issued At (iat) timestamp cannot be in the future', 403);
+        if (Utils::isFuture( $claims->get( 'iat' )->getTimestamp(),$leeway )) {
+            throw new TokenInvalidException( 'Issued At (iat) timestamp cannot be in the future',403 );
         }
 
-        if (Utils::isPast($claims->get('exp')->getTimestamp(), $leeway)) {
+        if (Utils::isPast( $claims->get( 'exp' )->getTimestamp(),$leeway )) {
             if ($this->config->getAutoRefresh()) {
-                if (Utils::isPast($claims->get('iat')->getTimestamp() + $this->config->getRefreshTTL(), $leeway)) {
-                    $this->token = $this->automaticRenewalToken();
+                if (Utils::isPast( $claims->get( 'iat' )->getTimestamp() + $this->config->getRefreshTTL(),$leeway )) {
+                    $this->automaticRenewalToken();
                 } else {
-                    throw new TokenRefreshExpiredException('The token is refresh expired', 402);
+                    throw new TokenRefreshExpiredException( 'The token is refresh expired',402 );
                 }
             }
-            throw new TokenExpiredException('The token is expired.', 401);
+            throw new TokenExpiredException( 'The token is expired.',401 );
         }
 
-        return $this->claimsToArray($claims->all());
+        return $this->claimsToArray( $claims->all() );
     }
 
     /**
@@ -181,15 +181,15 @@ class Jwt
      */
     private function claimsToArray(array $claims)
     {
-        return collect($claims)
-            ->map(function ($claim) {
-                if (is_a($claim, \DateTimeImmutable::class)) {
+        return collect( $claims )
+            ->map( function ($claim) {
+                if (is_a( $claim,\DateTimeImmutable::class )) {
                     return $claim->getTimestamp();
                 }
-                return is_object($claim) && method_exists($claim, 'getValue')
+                return is_object( $claim ) && method_exists( $claim,'getValue' )
                     ? $claim->getValue()
                     : $claim;
-            })
+            } )
             ->toArray();
     }
 
@@ -201,12 +201,12 @@ class Jwt
     {
         try {
             $claims     = $this->token->claims()->all();
-            $jti        = explode(':', $claims['jti']);
-            $identifier = end($jti);
-            unset($claims['iss'], $claims['iat'], $claims['nbf'], $claims['exp'], $claims['jti'], $claims['sub']);
-            return $this->make($identifier, $claims);
-        } catch (JwtException $e) {
-            throw new JwtException($e->getMessage(), $e->getCode(), $e->getPrevious());
+            $jti        = explode( ':',$claims['jti'] );
+            $identifier = end( $jti );
+            unset( $claims['iss'],$claims['iat'],$claims['nbf'],$claims['exp'],$claims['jti'],$claims['sub'] );
+            return $this->make( $identifier,$claims );
+        } catch ( JwtException $e ) {
+            throw new JwtException( $e->getMessage(),$e->getCode(),$e->getPrevious() );
         }
     }
 
@@ -214,25 +214,26 @@ class Jwt
     /**
      * Token 自动续期
      *
-     * @return Token
+     * @return mixed
      */
-    public function automaticRenewalToken(): Token
+    public function automaticRenewalToken()
     {
         $claims = $this->token->claims()->all();
 
-        $jti = explode(':', $claims['jti']);
-        unset($claims['iss'], $claims['jti'], $claims['iat'], $claims['nbf'], $claims['exp'], $claims['sub']);
+        $jti = explode( ':',$claims['jti'] );
+        unset( $claims['iss'],$claims['jti'],$claims['iat'],$claims['nbf'],$claims['exp'],$claims['sub'] );
 
-        $identifier = end($jti);
-        $token     = $this->make($identifier, $claims);
-        $refreshAt = $this->config->getRefreshTTL();
+        $identifier = end( $jti );
+        $token      = $this->make( $identifier,$claims );
+        $refreshAt  = $this->config->getRefreshTTL();
 
-        response()->withHeaders([
+        $this->token = $token;
+
+        return response()->withHeaders( [
             'Access-Control-Expose-Headers'     => 'Automatic-Renewal-Token,Automatic-Renewal-Token-RefreshAt',
             'Automatic-Renewal-Token'           => $token->toString(),
             'Automatic-Renewal-Token-RefreshAt' => $refreshAt
-        ]);
-        return $token;
+        ] );
     }
 
     /**
@@ -258,6 +259,6 @@ class Jwt
             return $this->token;
         }
 
-        throw new JwtException('Not logged in', 404);
+        throw new JwtException( 'Not logged in',404 );
     }
 }
